8953a168339601a36e4d49d5747709eecc1a8dfd
Thalita Priori <2022211510106@iesp.edu.br>
2022-10-23 20:29:00 -0300

    Perguntas
    Adicionando as perguntas as questoes.

--- Q10.py
+++ Q10.py
@@ -1,4 +1,4 @@
-#Escreva um programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
+#Q10-Escreva um programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
 
 from num2words import num2words
 numero = int( input('Digite um número de  0 á 99 : ') )
